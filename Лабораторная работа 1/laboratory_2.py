f81aabcf196d8eb93f6c395417b62272d115fc3b
DariaIGrigoreva <147330642+DariaIGrigoreva@users.noreply.github.com>
2023-10-17 19:24:33 +0300

    Update laboratory_2.py

--- Лабораторная работа 1/laboratory_2.py
+++ Лабораторная работа 1/laboratory_2.py
@@ -1,16 +1,6 @@
-# TODO Найдите количество книг, которое можно разместить на дискете
-Volume_in_mbytes = 1.44
-Pages = 100
-lines = 50
-words = 25
-safe = 4
-Volume_in_bytes = Volume_in_mbytes * 1024 ** 2
+inf = [1.44, 100, 50, 25, 4]
 
-count_of_words = Pages * lines * words
-
-safe_book = count_of_words * safe
-
-count_of_book = Volume_in_bytes / safe_book
-count_of_book_ = round(count_of_book)
+count_of_book = inf[0] * 1024 ** 2 / inf[1] / inf[2] / inf[3] / inf[4]
+count_of_book_ = int(count_of_book)
 
 print("Количество книг, помещающихся на дискету:", count_of_book_)
